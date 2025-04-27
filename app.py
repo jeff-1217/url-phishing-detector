2b49e671f61861cb5eac69fd5428a90a61639650
jefrine correya <jefrinecorreya1234@gmail.com>
2025-04-27 14:54:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def index():
             'threat_matches': data.get('matches', [])
         }
     return render_template('index.html', result=result)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+        import os
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
